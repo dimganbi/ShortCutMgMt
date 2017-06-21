db1588ec56181de6c635df9250c272384c039450
ex2tron <ex2tron@foxmail.com>
2017-06-21 16:24:58 +0800

    Init Project 2017-06-21

---
+++ ShortMgmt/frmShortMain.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ShortMgmt
+{
+    partial class frmShortMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvRegeditData = new System.Windows.Forms.DataGridView();
+            this.btnAddShort = new System.Windows.Forms.Button();
+            this.txtShortName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSelectPath = new System.Windows.Forms.TextBox();
+            this.btnSelectTarget = new System.Windows.Forms.Button();
+            this.linklblSelectPath = new System.Windows.Forms.LinkLabel();
+            this.btnDeleteSelect = new System.Windows.Forms.Button();
+            this.radiobtnFile = new System.Windows.Forms.RadioButton();
+            this.radiobtnFolder = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegeditData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRegeditData
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRegeditData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvRegeditData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvRegeditData.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvRegeditData.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvRegeditData.Location = new System.Drawing.Point(0, 134);
+            this.dgvRegeditData.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dgvRegeditData.Name = "dgvRegeditData";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRegeditData.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvRegeditData.RowTemplate.Height = 21;
+            this.dgvRegeditData.Size = new System.Drawing.Size(1172, 378);
+            this.dgvRegeditData.TabIndex = 5;
+            // 
+            // btnAddShort
+            // 
+            this.btnAddShort.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddShort.Location = new System.Drawing.Point(940, 27);
+            this.btnAddShort.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnAddShort.Name = "btnAddShort";
+            this.btnAddShort.Size = new System.Drawing.Size(100, 28);
+            this.btnAddShort.TabIndex = 3;
+            this.btnAddShort.Text = "添加";
+            this.btnAddShort.UseVisualStyleBackColor = true;
+            this.btnAddShort.Click += new System.EventHandler(this.btnAddShort_Click);
+            // 
+            // txtShortName
+            // 
+            this.txtShortName.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtShortName.Location = new System.Drawing.Point(153, 27);
+            this.txtShortName.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtShortName.Name = "txtShortName";
+            this.txtShortName.Size = new System.Drawing.Size(132, 27);
+            this.txtShortName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "快捷名称：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 72);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "执行目标：";
+            // 
+            // txtSelectPath
+            // 
+            this.txtSelectPath.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSelectPath.Location = new System.Drawing.Point(153, 67);
+            this.txtSelectPath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtSelectPath.Name = "txtSelectPath";
+            this.txtSelectPath.Size = new System.Drawing.Size(445, 27);
+            this.txtSelectPath.TabIndex = 1;
+            // 
+            // btnSelectTarget
+            // 
+            this.btnSelectTarget.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSelectTarget.Location = new System.Drawing.Point(606, 62);
+            this.btnSelectTarget.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSelectTarget.Name = "btnSelectTarget";
+            this.btnSelectTarget.Size = new System.Drawing.Size(117, 32);
+            this.btnSelectTarget.TabIndex = 2;
+            this.btnSelectTarget.Text = "浏览文件";
+            this.btnSelectTarget.UseVisualStyleBackColor = true;
+            this.btnSelectTarget.Click += new System.EventHandler(this.btnSelectTarget_Click);
+            // 
+            // linklblSelectPath
+            // 
+            this.linklblSelectPath.AutoSize = true;
+            this.linklblSelectPath.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linklblSelectPath.Location = new System.Drawing.Point(151, 103);
+            this.linklblSelectPath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linklblSelectPath.Name = "linklblSelectPath";
+            this.linklblSelectPath.Size = new System.Drawing.Size(0, 20);
+            this.linklblSelectPath.TabIndex = 5;
+            this.linklblSelectPath.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linklblSelectPath_LinkClicked);
+            // 
+            // btnDeleteSelect
+            // 
+            this.btnDeleteSelect.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteSelect.Location = new System.Drawing.Point(940, 67);
+            this.btnDeleteSelect.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnDeleteSelect.Name = "btnDeleteSelect";
+            this.btnDeleteSelect.Size = new System.Drawing.Size(100, 28);
+            this.btnDeleteSelect.TabIndex = 4;
+            this.btnDeleteSelect.Text = "删除";
+            this.btnDeleteSelect.UseVisualStyleBackColor = true;
+            this.btnDeleteSelect.Click += new System.EventHandler(this.btnDeleteSelect_Click);
+            // 
+            // radiobtnFile
+            // 
+            this.radiobtnFile.AutoSize = true;
+            this.radiobtnFile.Checked = true;
+            this.radiobtnFile.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radiobtnFile.Location = new System.Drawing.Point(405, 29);
+            this.radiobtnFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radiobtnFile.Name = "radiobtnFile";
+            this.radiobtnFile.Size = new System.Drawing.Size(60, 24);
+            this.radiobtnFile.TabIndex = 6;
+            this.radiobtnFile.TabStop = true;
+            this.radiobtnFile.Text = "文件";
+            this.radiobtnFile.UseVisualStyleBackColor = true;
+            this.radiobtnFile.CheckedChanged += new System.EventHandler(this.radiobtnFile_CheckedChanged);
+            // 
+            // radiobtnFolder
+            // 
+            this.radiobtnFolder.AutoSize = true;
+            this.radiobtnFolder.Location = new System.Drawing.Point(520, 31);
+            this.radiobtnFolder.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radiobtnFolder.Name = "radiobtnFolder";
+            this.radiobtnFolder.Size = new System.Drawing.Size(73, 19);
+            this.radiobtnFolder.TabIndex = 6;
+            this.radiobtnFolder.Text = "文件夹";
+            this.radiobtnFolder.UseVisualStyleBackColor = true;
+            this.radiobtnFolder.CheckedChanged += new System.EventHandler(this.radiobtnFolder_CheckedChanged);
+            // 
+            // frmShortMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 512);
+            this.Controls.Add(this.radiobtnFolder);
+            this.Controls.Add(this.radiobtnFile);
+            this.Controls.Add(this.btnDeleteSelect);
+            this.Controls.Add(this.linklblSelectPath);
+            this.Controls.Add(this.btnSelectTarget);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSelectPath);
+            this.Controls.Add(this.txtShortName);
+            this.Controls.Add(this.btnAddShort);
+            this.Controls.Add(this.dgvRegeditData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "frmShortMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "快捷命令管理工具Ver0.2";
+            this.Load += new System.EventHandler(this.frmShortMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegeditData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRegeditData;
+        private System.Windows.Forms.Button btnAddShort;
+        private System.Windows.Forms.TextBox txtShortName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSelectPath;
+        private System.Windows.Forms.Button btnSelectTarget;
+        private System.Windows.Forms.LinkLabel linklblSelectPath;
+        private System.Windows.Forms.Button btnDeleteSelect;
+        private System.Windows.Forms.RadioButton radiobtnFile;
+        private System.Windows.Forms.RadioButton radiobtnFolder;
+    }
+}
+
---
+++ ShortMgmt/frmShortMain.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Collections;
+using IWshRuntimeLibrary;
+using System.IO;
+
+namespace ShortMgmt
+{
+    public partial class frmShortMain : Form
+    {
+        public frmShortMain()
+        {
+            InitializeComponent();
+            dgvRegeditData.SelectionMode = DataGridViewSelectionMode.FullRowSelect;//行选择模式
+            dgvRegeditData.ReadOnly = true;//只读，不可编辑
+            dgvRegeditData.ColumnCount = 2;//两列数据
+            dgvRegeditData.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;//第二列填充数据表
+            dgvRegeditData.ColumnHeadersVisible = true;//显示列标题
+            dgvRegeditData.Columns[0].Name = "快捷命令";//第一列的标题
+            dgvRegeditData.Columns[1].Name = "程序位置";//第二列的标题
+        }
+
+        string shortName = string.Empty;//要添加的快捷命令名称
+        string shortFilePath = string.Empty;//目标文件的上层路径
+        string shortFilePathWithName = string.Empty;//目标文件路径
+        string selectFilePath = string.Empty;
+        string selectFileName = string.Empty;//获取目标文件名称
+        string shortFolderPath = string.Empty;//目标文件夹的名称
+        string regeditPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";//注册表路径
+        Hashtable keyPorps = null;
+        RegistryKey baseKey = null;
+        RegistryKey shortKeys = null;
+        bool isFileOrFolder = false;//true->文件夹，false->文件
+
+        private void frmShortMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                showRegeditData();//加载注册表数据
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 加载注册表数据
+        /// </summary>
+        public void showRegeditData()
+        {
+            dgvRegeditData.Rows.Clear();
+            keyPorps = new Hashtable();
+            baseKey = Registry.LocalMachine;//基键
+            shortKeys = baseKey.OpenSubKey(regeditPath, true);
+            //List<string> keyNames = new List<string>();
+            //List<string> keyPaths = new List<string>();
+            try
+            {
+                foreach (string keyName in shortKeys.GetSubKeyNames())
+                {
+                    RegistryKey shortKey = shortKeys.OpenSubKey(keyName);
+                    //keyPaths.Add(shortKey.GetValue("").ToString());
+                    //keyNames.Add(keyName);
+                    if (shortKey.GetValue("") != null)
+                    {
+                        keyPorps.Add(keyName.Substring(0, keyName.LastIndexOf(".")), shortKey.GetValue("").ToString());
+                    }
+                    else
+                    {
+                        keyPorps.Add(keyName.Substring(0, keyName.LastIndexOf(".")), "无效");
+                    }
+                    // MessageBox.Show(shortKey.GetValue("").ToString());
+                }
+
+                //遍历keyPorp来填充DataGridView中的行数据
+                foreach (DictionaryEntry keyProp in keyPorps)
+                {
+                    string[] keys = new string[] { keyProp.Key.ToString(), keyProp.Value.ToString() };
+                    dgvRegeditData.Rows.Add(keys);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 打开选择的路径
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linklblSelectPath_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linklblSelectPath.Text);//打开该路径
+        }
+
+        /// <summary>
+        /// 添加快捷命令
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddShort_Click(object sender, EventArgs e)
+        {
+            if (txtShortName.Text == string.Empty || txtSelectPath.Text == string.Empty)
+            {
+                MessageBox.Show("快捷名称或文件路径不可为空！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (isFileOrFolder)
+                {
+                    addFolderShort();
+                }
+                else
+                {
+                    addFileShort();
+                }
+                showRegeditData();
+                txtShortName.Text = txtSelectPath.Text = string.Empty;
+            }
+
+        }
+
+        private void btnDeleteSelect_Click(object sender, EventArgs e)
+        {
+            DialogResult userChoose = MessageBox.Show("该项可能涉及系统内部运行，你确定要删除吗？", "警告", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (userChoose == DialogResult.Yes)
+            {
+                for (int i = 0; i < dgvRegeditData.SelectedRows.Count; i++)
+                {
+                    shortKeys.DeleteSubKey(dgvRegeditData.SelectedRows[i].Cells[0].Value.ToString() + ".exe", false);
+                    showRegeditData();
+                }
+            }
+            else
+            {
+                //Do Dothing
+            }
+        }
+
+        /// <summary>
+        /// 创建文件快捷方式
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void createFileShortcut(string fileName, string fileNamePath)
+        {
+            if (!Directory.Exists(System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts"))
+            {
+                Directory.CreateDirectory(System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts");
+            }
+            string shortcutPath = System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts" + "\\" + fileName + ".lnk";
+            try
+            {
+                IWshRuntimeLibrary.WshShell shell = new WshShell();
+                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+                shortcut.TargetPath = fileNamePath;
+                shortcut.Save();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 创建文件夹快捷方式
+        /// </summary>
+        /// <param name="folderPath"></param>
+        public void createFolderShortcut(string folderPath)
+        {
+            if (!Directory.Exists(System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts"))
+            {
+                Directory.CreateDirectory(System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts");
+            }
+            string shortcutPath = System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts" + "\\" + txtShortName.Text.Trim() + ".lnk";
+            try
+            {
+                IWshRuntimeLibrary.WshShell shell = new WshShell();
+                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+                shortcut.TargetPath = folderPath;
+                shortcut.Save();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSelectTarget_Click(object sender, EventArgs e)
+        {
+            if (isFileOrFolder)
+            {
+                openFolderDialog();
+            }
+            else
+            {
+                openFileDialog();
+            }
+
+        }
+
+        private void radiobtnFile_CheckedChanged(object sender, EventArgs e)
+        {
+            isFileOrFolder = false;
+            btnSelectTarget.Text = "浏览文件";
+        }
+
+        private void radiobtnFolder_CheckedChanged(object sender, EventArgs e)
+        {
+            isFileOrFolder = true;
+            btnSelectTarget.Text = "浏览文件夹";
+        }
+
+        public void openFileDialog()
+        {
+            OpenFileDialog newRunFile = new OpenFileDialog();
+            newRunFile.Title = "选择要执行的文件";
+            newRunFile.Filter = "可执行程序(*.exe)|*.exe";
+            if (newRunFile.ShowDialog() == DialogResult.OK)
+            {
+                selectFileName = newRunFile.SafeFileName.Substring(0, newRunFile.SafeFileName.LastIndexOf("."));
+                txtSelectPath.Text = selectFilePath = newRunFile.FileName;//获取文件路径
+                linklblSelectPath.Text = newRunFile.FileName.Substring(0, newRunFile.FileName.LastIndexOf("\\"));//获取文件夹路径
+            }
+        }
+
+        public void openFolderDialog()
+        {
+            FolderBrowserDialog newFolder = new FolderBrowserDialog();
+            if (newFolder.ShowDialog() == DialogResult.OK)
+            {
+                txtSelectPath.Text = linklblSelectPath.Text = shortFolderPath = newFolder.SelectedPath;
+            }
+        }
+
+        public void addFileShort()
+        {
+            try
+            {
+                createFileShortcut(selectFileName, selectFilePath);
+                string localShortPath = System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts";
+                string localShortName = System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts" + "\\" + selectFileName + ".lnk";
+                //创建子键
+                RegistryKey newShortKey = shortKeys.CreateSubKey(txtShortName.Text.Trim() + ".exe");
+                //设定默认值为目标文件路径
+                newShortKey.SetValue("", localShortName);
+                //设定Path键/值
+                newShortKey.SetValue("Path", localShortPath);
+                MessageBox.Show(txtShortName.Text.Trim() + "添加成功，可在运行对话框中输入" + txtShortName.Text.Trim() + "运行程序", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void addFolderShort()
+        {
+            try
+            {
+                createFolderShortcut(shortFolderPath);
+                string localShortPath = System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts";
+                string localShortName = System.IO.Directory.GetCurrentDirectory() + @"\Shortcuts" + "\\" + txtShortName.Text.Trim() + ".lnk";
+                //创建子键
+                RegistryKey newShortKey = shortKeys.CreateSubKey(txtShortName.Text.Trim() + ".exe");
+                //设定默认值为目标文件路径
+                newShortKey.SetValue("", localShortName);
+                //设定Path键/值
+                newShortKey.SetValue("Path", localShortPath);
+                MessageBox.Show(txtShortName.Text.Trim() + "添加成功，可在运行对话框中输入" + txtShortName.Text.Trim() + "运行程序", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
